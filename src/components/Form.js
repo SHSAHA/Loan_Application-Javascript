6523d7b63a1c137f4d529053f1a65fbaa07f9d5c
Shaswata Saha <shsaha@ttu.ee>
2019-05-30 07:48:01 +0300

    files

---
+++ src/components/Form.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+
+
+
+class Form extends Component {
+     state = { 
+          amount: '',
+          term : ''
+     }
+
+     handleSubmit = (e) => {
+          e.preventDefault();
+
+          // read the values from the state
+          const {amount, term} = this.state;
+
+          // pass the data to the main component
+
+          this.props.loanInformation(amount, term);
+     }
+
+     handleChange = (e) => {
+          // read form data
+          const {name, value} = e.target;
+
+          // update the state
+          this.setState({
+               [name] : Number(value)
+          })
+     }
+
+     validateForm = () => {
+          // destructuring
+          const {amount, term} = this.state;
+
+          const notValid = !amount || !term;
+
+          // console.log(notValid);
+
+          return notValid;
+     }
+
+     render() { 
+
+          // extract some contents from the state
+          // const {amount} = this.state;
+
+          return ( 
+               <form onSubmit={this.handleSubmit}>
+                    <div>
+                         <label>Amount</label>
+                         <input 
+                              onChange={this.handleChange}
+                              id="name" 
+                              name="amount" 
+                              className="u-full-width" 
+                              type="number" 
+                              placeholder="Eg: 3000" />
+                    </div>
+                    <div>
+                         <label>Months to Repay</label>
+                         <select 
+                              onChange={this.handleChange}
+                              name="term" 
+                              className="u-full-width">
+                                   <option value="">Select</option>
+                                   <option value="3">3 Months</option>
+                                   <option value="6">6 Months</option>
+                                   <option value="12">12 Months</option>
+                                   <option value="24">24 Months</option>
+                         </select>
+                    </div>
+                    <div>
+                         <input disabled={ this.validateForm() }
+                                type="submit" 
+                                value="Calculate" 
+                                className="u-full-width button-primary" />
+                    </div>
+               </form>
+           );
+     }
+}
+ 
+export default Form;
---
+++ src/components/Message.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Message = () => (
+     <p>Add an Amount and the Term to Repay</p>
+)
+export default Message;
